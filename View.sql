8ecf67299d2b8d1d5330532a55f5952fcc0fa881
nvt-10-10 <nvtuyen10102002@gmail.com>
2023-02-07 19:50:06 +0700

    Nhom 3

---
+++ View.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE VIEW VIEW_DEMO 
+AS
+SELECT em.employee_id, CONCAT(em.first_name , ' ' ,em.last_name) 
+as full_name, em.hire_date, dep.department_name 
+FROM employees em JOIN departments dep
+ON em.department_id = dep.department_id
+
+SELECT * FROM VIEW_DEMO
